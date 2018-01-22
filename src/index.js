96800aaffbc97dab2f759a37296aa9ecdafd55d0
s-o-u-r-c-e <michal.tracy@outlook.com>
2018-01-22 18:41:34 +0000

    add mongoose

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cars from './routes/car';
+import mongoose from 'mongoose';
 
+mongoose.connect('mongodb://localhost/cars');
 const app = express();
 
 app.use(bodyParser.json());
--- src/routes/car.js
+++ src/routes/car.js
@@ -1,47 +1,71 @@
 import _ from 'lodash';
+import Car from '../models/car';
 
 module.exports = function(app) {
-    const _cars = [];
 
     /* Create */
     app.post('/car', (req, res) => {
-        _cars.push(req.body);
-        res.json({info: 'car created successfully'});
+        const newCar = new Car(req.body);
+        newCar.save((err) => {
+           if (err) {
+               res.json({info: 'error during car creation', error: err});
+           }
+           res.json({info: 'car created successfully'});
+        });
     });
 
     /* Read */
-    app.get('/cars', (req, res) => {
-        res.send(_cars);
+    app.get('/car', (req, res) => {
+        Car.find(req.params.id, (err, cars) => {
+           if (err) {
+               res.json({info: 'error during find car', error: err});
+           }
+           res.json({info: 'cars found successfully', data: cars});
+        });
     });
 
     app.get('/car/:id', (req, res) => {
-        res.send(
-            _.find(
-                _cars,
-                {
-                    name: req.params.id
-                }
-            )
-        );
+        Car.findById(req.params.id, (err, car) => {
+           if (err) {
+               res.json({info: 'error during find car', error: err});
+           }
+           if (car) {
+               res.json({info: 'car found successfully', data: car});
+           } else {
+               res.json({info: 'car not found'});
+           }
+
+        });
     });
 
     /* Update */
     app.put('/car/:id', (req, res) => {
-        const idx = _.findIndex(
-            _cars,
-            {
-                name: req.params.id
+        Car.findById(req.params.id, (err, car) => {
+            if (err) {
+                res.json({info: 'error during find car', error: err});
             }
-        );
-        _.merge(_cars[idx], req.body);
-        res.json({info: 'car updated successfully'});
+            if (car) {
+                _.merge(car, req.body);
+                car.save((err) => {
+                   if (err) {
+                       res.json({info: 'error during car update', error: err});
+                   }
+                   res.json({info: 'car updated successfully'});
+                });
+            } else {
+                res.json({info: 'car not found'});
+            }
+        });
     });
 
     /* Delete */
     app.delete('/car/:id', (req, res) => {
-        _.remove(_cars, (car) => {
-            return car.name === req.params.id;
+        Car.findByIdAndRemove(req.params.id, (err) => {
+           if (err) {
+               res.json({info: 'error during remove car', error: err});
+           }
+            res.json({info: 'car removed successfully'});
         });
-        res.json({info: 'car removed successfully'});
+
     });
 };
